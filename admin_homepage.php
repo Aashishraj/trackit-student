2c2ccfb363695ef5ffd039ba14c5566994a22e62
Aashish Raj <47362045+Aashishraj@users.noreply.github.com>
2020-05-23 08:21:10 +0530

    Update admin_homepage.php

--- admin_homepage.php
+++ admin_homepage.php
@@ -177,7 +177,7 @@ aria-label="Search" >
                      
                 <?php  
                 session_start();
-                $con = mysqli_connect("https://www.db4free.net/phpMyAdmin", "aashishraj", "neopassword");  
+                $con = mysqli_connect("db4free.net", "aashishraj", "neopassword");  
                     if(!$con)
 	                {die('could not connectr'.mysqli_error($con));}
                     mysqli_select_db($con,"trackit_student");
