a67e7d89e6291be2086c1fcb7d86730b58bf8b38
Aashish Raj <47362045+Aashishraj@users.noreply.github.com>
2020-05-23 10:00:26 +0530

    Update student_login.php

--- student_login.php
+++ student_login.php
@@ -137,7 +137,7 @@ if(isset($_POST['submit']))
 	}
 	else{
 	session_start();
-	$con = mysqli_connect("db4free.net", "aashishraj", "neopassword");  
+	$con = mysqli_connect("db4free.net", "aashishraj", "hello123");  
                     if(!$con)
 	                {die('could not connectr'.mysqli_error($con));}
                     mysqli_select_db($con,"trackit_student");
